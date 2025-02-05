1574eec62f9f122dcd431428ba1dee5a4a8c207c
Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
2025-02-05 12:42:50 +0100

    Update proxmox-mail-gateway.sh

--- ct/proxmox-mail-gateway.sh
+++ ct/proxmox-mail-gateway.sh
@@ -3,7 +3,7 @@ source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/m
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: thost96 (thost96)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Spurce: https://www.proxmox.com/en/products/proxmox-mail-gateway
+# Source: https://www.proxmox.com/en/products/proxmox-mail-gateway
 
 APP="Proxmox-Mail-Gateway"
 var_tags="mail"
